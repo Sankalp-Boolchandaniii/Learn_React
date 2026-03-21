d62249990dfe489c22c18869c9b105e001e552b1
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2026-03-22 00:30:32 +0530

    02-component

---
+++ 02-component/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h2>Sankalp</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do.</p>
+    </div>
+  );
+};
+
+export default Card;
