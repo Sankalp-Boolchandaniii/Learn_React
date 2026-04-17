a6d50f875d09444c4b4fb51426f2b7d7695961ff
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2026-04-18 02:22:49 +0530

    props project

--- 03-props/src/components/Card.jsx
+++ 03-props/src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Card(props) {
+function Card(user, age) {
     return (
         <div>
             <h1>Card</h1>
-            <p>Name: {props.user}</p>
-            <p>Age: {props.age}</p>
+            <p>Name: {user}</p>
+            <p>Age: {age}</p>
         </div>
     )
 }
