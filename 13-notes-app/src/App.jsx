49fa75e200df76851dc84c89122f5acd79c6d126
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2026-06-20 02:00:33 +0530

    13-notes-app

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -7,13 +7,17 @@ const App = () => {
 
   function submitHandle(e) {
     e.preventDefault();
-    setTask((prev) => [...prev, { title, desc }]);
+    let copyTask = [...task];
+    copyTask.push({ title, desc });
+    setTask(copyTask);
     setTitle("");
     setDesc("");
   }
 
-  function deleteNote(id) {
-    setTask((prev) => prev.splice(id, 1));
+  function deleteNote(idx) {
+    let copyTask = [...task];
+    copyTask.splice(idx, 1);
+    setTask(copyTask);
   }
 
   return (
@@ -24,47 +28,56 @@ const App = () => {
         }}
         className="flex gap-4 lg:w-1/2 p-10 flex-col items-start"
       >
-        <h1 className="text-4xl font-bold">Add Notes</h1>
+        <h1 className="text-4xl mb-2 font-bold">Add Notes</h1>
+
         <input
-          type="text"
           placeholder="Enter Notes Heading"
-          className="px-5 w-full font-medium py-2 border-2 outline-none rounded"
+          className="px-5 w-full font-medium py-2 border-2 outline-none rounded "
           value={title}
           onChange={(e) => {
             setTitle(e.target.value);
           }}
         />
+
         <textarea
-          name="text"
-          className="px-5 w-full font-medium h-32 py-2 flex item-start flex-row border-2 outline-none rounded"
-          placeholder="Write Details"
+          type="text"
+          className="px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none  rounded "
+          placeholder="Write Details here"
           value={desc}
           onChange={(e) => {
             setDesc(e.target.value);
           }}
         />
-        <button className="bg-white font-medium w-full outline-none text-black px-5 py-2 rounded">
+
+        <button className="bg-white active:scale-95 font-medium w-full outline-none  text-black px-5 py-2 rounded">
           Add Note
         </button>
       </form>
-      <div className="lg:w-1/2 lg:border-l-2 p-10">
+      <div className="lg:w-1/2 lg:border-l-2  p-10">
         <h1 className="text-4xl font-bold">Recent Notes</h1>
-        <div className="flex flex-wrap gap-5 mt-5 justify-start h-full overflow-auto">
+        <div className="flex flex-wrap items-start justify-start gap-5 mt-6 h-[90%] overflow-auto">
           {task.map(function (elem, idx) {
             return (
-              <div key={idx} className="h-52 w-40 rounded-2xl bg-white">
-                <div className="bg-amber-400 w-full h-full flex flex-col">
-                  <h3 className="bg-blue-300 text-black">{elem.title}</h3>
-                  <p className="text-black p-5">{elem.desc}</p>
-                  <button
-                    onClick={() => {
-                      deleteNote(idx);
-                    }}
-                    className="border-2 bg-white text-red-400"
-                  >
-                    Delete
-                  </button>
+              <div
+                key={idx}
+                className=" flex justify-between flex-col items-start relative h-52 w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]"
+              >
+                <div>
+                  <h3 className="leading-tight text-lg font-bold">
+                    {elem.title}
+                  </h3>
+                  <p className="mt-2 leading-tight text-xs font-semibold text-gray-600">
+                    {elem.desc}
+                  </p>
                 </div>
+                <button
+                  onClick={() => {
+                    deleteNote(idx);
+                  }}
+                  className="w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white"
+                >
+                  Delete
+                </button>
               </div>
             );
           })}
